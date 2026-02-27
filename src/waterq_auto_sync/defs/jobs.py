24f40063ee3c4acae687a724ca2db8a452b5565c
darespinoza <despinoza@uazuay.edu.ec>
2026-02-27 15:20:37 -0500

    [ADD] Uploading BWMP data to IERSE database

--- src/waterq_auto_sync/defs/jobs.py
+++ src/waterq_auto_sync/defs/jobs.py
@@ -1,13 +1,15 @@
 import dagster as dg
 from .assets import (
     pg_waterq_stations,
-    mfqb_data_raw
+    mfqb_data_raw,
+    mfqb_data_bronze,
+    mfqb_data_silver,
 )
 
 # Postgres to Minio job
 etapa_to_ierse_bmwp_job = dg.define_asset_job(
     name="etapa_to_ierse_bmwp_job",
-    selection=[pg_waterq_stations, mfqb_data_raw],
+    selection=[pg_waterq_stations, mfqb_data_raw, mfqb_data_bronze, mfqb_data_silver,],
 )
 
 
